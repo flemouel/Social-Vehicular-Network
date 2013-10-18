e168231d4b2dbdb9cbb5caabf4ac9a3d314756b4
malebre <marieange.lebre@gmail.com>
2013-10-18 17:12:39 +0200

    correct repertory

--- AppliConnect/generateTrip.sh
+++ AppliConnect/generateTrip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/Documents/Sumo/sumo-0.16.0/tools/trip/randomTrips.py -n data/map.net.xml -o data/map.trips.xml -e $1
+~usr/share/tools/trip/randomTrips.py -n data/map.net.xml -o data/map.trips.xml -e $1
