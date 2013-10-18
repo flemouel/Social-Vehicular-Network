686e731a370122e94512df6962b12844b2df192e
malebre <marieange.lebre@gmail.com>
2013-10-18 17:14:42 +0200

    Update generateTrip.sh

--- AppliConnect/generateTrip.sh
+++ AppliConnect/generateTrip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~usr/share/tools/trip/randomTrips.py -n data/map.net.xml -o data/map.trips.xml -e $1
+~/usr/share/tools/trip/randomTrips.py -n data/map.net.xml -o data/map.trips.xml -e $1
